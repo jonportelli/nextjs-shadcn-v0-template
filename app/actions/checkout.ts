1480c84f32f5111ec3d63669aa38e6bcecb2fe7f
Jonathan Portelli <jonportelli@gmail.com>
2024-12-07 08:00:28 +0000

    fix api version 2023-10-16

--- app/actions/checkout.ts
+++ app/actions/checkout.ts
@@ -4,7 +4,7 @@ import { redirect } from 'next/navigation'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2024-11-20.acacia',
 })
 
 export async function handleCheckout(pkg: { name: string; price: number }) {
